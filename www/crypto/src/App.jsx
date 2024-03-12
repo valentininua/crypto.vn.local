4380a86a26a628eac5fbe8b2acaab957bac6a064
Valentin B <work@valentin.in.ua>
2024-03-12 03:07:32 +0300

    bootstrap

--- www/crypto/src/App.jsx
+++ www/crypto/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
 import './App.css'
 
 function App() {
